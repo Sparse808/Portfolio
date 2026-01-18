4a2dc2a72ccce3c4b7a267b187e3907aaacd2b80
Sparse808 <oblack.dev@gmail.com>
2026-01-17 20:19:16 -0500

    create project structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const {DateTime} = require("luxon");
+
 module.exports = function (eleventyConfig) {
   // Copy static assets straight through to _site
   eleventyConfig.addPassthroughCopy("src/res");
   eleventyConfig.addPassthroughCopy("src/css");
+
+  eleventyConfig.addFilter("projectDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+  });
 
   return {
     dir: {
---
+++ src/Projects/temp.md
@@ -0,0 +1,16 @@
+---
+title: Temp Project
+author: Walker McKee
+tags: ['project', 'featured']
+date: 2026-01-17
+image : /res/ProfilePicture.jpg
+---
+
+# Hello
+
+fdfdsfsd
+fsdfdsfsd
+fdsfsdf
+
+### End
+fdfsdfsd
